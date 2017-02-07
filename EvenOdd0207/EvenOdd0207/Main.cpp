c6e1fd0532f30dbd4d3093528ec2ae5b7a9a3b54
Game110AM <Game110AM@605-110-STU19>
2017-02-07 07:51:33 -0700

    commit

---
+++ EvenOdd0207/EvenOdd0207/Main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+
+	int age;
+	cout << "How old are you?" << endl;
+	cin >> age;
+	if (age % 2 == 0)
+		cout << "Is your name Even Steven?" << endl;
+	else
+		cout << "You're an Odd Ball!" << endl;
+}
