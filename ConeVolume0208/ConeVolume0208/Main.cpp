d754023a6273309469db65a9c0e24ca408711cba
Game110AM <Game110AM@605-110-STU19>
2017-02-08 07:51:13 -0700

    commit

---
+++ ConeVolume0208/ConeVolume0208/Main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main () {
+
+	int base;
+	int height;
+
+	cout << "Enter the base of the cone!" << endl;
+	cin >> base;
+	cout << "Enter the height of the cone!" << endl;
+	cin >> height;
+	cout << "The volume of the cone is " << (base*height) / 3 << "!" << endl;
+
+}
