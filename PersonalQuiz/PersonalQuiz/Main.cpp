05e1f21d2a40616b83281575346b988a54b9e5fa
Game110AM <Game110AM@605-110-STU19>
2017-02-13 09:04:23 -0700

    commit

--- PersonalQuiz/PersonalQuiz/Main.cpp
+++ PersonalQuiz/PersonalQuiz/Main.cpp
@@ -8,6 +8,8 @@ int main() {
 	int cat = 0;
 	int snail = 0;
 
+	cout << "Welcome to the 'What aniaml are you' quiz!" << endl;
+
 	cout << "What is your favorite color? (pink=p, blue=b, black=l, green=g)" << endl;
 	cin >> input;
 	if (input == 'p')
@@ -30,13 +32,13 @@ int main() {
 	else if (input == 'c')
 		snail = snail + 1;
 
-	cout << "What is your favorite food? (beef=b, fish=f, tuna=t, salad=s)" << endl;
+	cout << "What is your favorite food? (beef=b, fish=f, chicken=c, salad=s)" << endl;
 	cin >> input;
 	if (input == 'b')
 		dog = dog + 1;
 	else if (input == 'f')
 		whale = whale + 1;
-	else if (input == 't')
+	else if (input == 'c')
 		cat = cat + 1;
 	else if (input == 's')
 		snail = snail + 1;
@@ -50,17 +52,6 @@ int main() {
 	else if (input == 'p')
 		cat = cat + 1;
 	else if (input == 'c')
-		snail = snail + 1;
-
-	cout << "What is your favorite color? (pink=p, blue=b, black=l, green=g)" << endl;
-	cin >> input;
-	if (input == 'p')
-		dog = dog + 1;
-	else if (input == 'b')
-		whale = whale + 1;
-	else if (input == 'l')
-		cat = cat + 1;
-	else if (input == 'g')
 		snail = snail + 1;
 
 	cout << "What is your eye color? (brown=b, black=l, hazel=h, green=g)" << endl;
@@ -117,5 +108,25 @@ int main() {
 		cat = cat + 1;
 	else if (input == 's')
 		snail = snail + 1;
+
+	cout << "What is your favorite movie genre? (action=a, horror=h, mystery=m, romance=r)" << endl;
+	cin >> input;
+	if (input == 'a')
+		dog = dog + 1;
+	else if (input == 'h')
+		whale = whale + 1;
+	else if (input == 'm')
+		cat = cat + 1;
+	else if (input == 'r')
+		snail = snail + 1;
+
+	if ((dog > whale) && (dog > cat) && (dog > snail))
+		cout << "You are a DOG!" << endl;
+	if ((whale > dog) && (whale > cat) && (whale > snail))
+		cout << "You are a WHALE!" << endl;
+	if ((cat > dog) && (cat > whale) && (cat > snail))
+		cout << "You are a CAT!" << endl;
+	if ((snail > dog) && (snail > whale) && (snail > cat))
+		cout << "You are a SNAIL!" << endl;
 
 }
