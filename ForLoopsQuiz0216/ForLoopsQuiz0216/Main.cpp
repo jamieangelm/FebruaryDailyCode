20f21cfe4dc9df32d83420e3273053f27dc83d6a
Game110AM <Game110AM@605-110-STU19>
2017-02-16 07:50:09 -0700

    commit

---
+++ ForLoopsQuiz0216/ForLoopsQuiz0216/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+	for (int i = 2; i < 10; i++)
+		cout << "hi" << endl;
+	for (int j = 7; j < 15; j++)
+		cout << "boi" << endl;
+	for (int k = 10; k > 5; k--)
+		cout << "same" << endl;
+}
