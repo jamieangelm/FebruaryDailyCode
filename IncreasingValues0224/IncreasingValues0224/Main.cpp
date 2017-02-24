fc8da6ae6766fde2230907f4200d30be8b3547a8
Game110AM <Game110AM@605-110-STU19>
2017-02-24 08:05:18 -0700

    commit

--- IncreasingValues0224/IncreasingValues0224/Main.cpp
+++ IncreasingValues0224/IncreasingValues0224/Main.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
+#include <iomanip>
 int main() {
 	double num = 1;
 	
 	for (int i = 0; i <= 11; i++) {
 		num *= 10;
-		cout << fixed << num << endl<<endl;
+		cout << setprecision(0) <<fixed << num << endl<<endl;
 	}
 }
