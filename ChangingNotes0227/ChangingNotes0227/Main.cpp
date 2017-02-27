2a501a87cf02029d556e398680b53117a4abf04d
Game110AM <Game110AM@605-110-STU19>
2017-02-27 07:59:22 -0700

    uhfgujh

--- ChangingNotes0227/ChangingNotes0227/Main.cpp
+++ ChangingNotes0227/ChangingNotes0227/Main.cpp
@@ -1,9 +1,11 @@
 #include <windows.h>
+#include <iostream>
 using namespace std;
 int main() {
 	int beep1 = 100;
 	int beep2 = 900;
 	system("Color 3F");
+		cout << "Happy Monday" << endl;
 		for (int j = 0; j < 7; j++) {
 			beep1 += 100;
 			Beep(beep1, beep2);
