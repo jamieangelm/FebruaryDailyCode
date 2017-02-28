302939d61df9bcb920bbab4407e64e17023211ad
Game110AM <Game110AM@605-110-STU19>
2017-02-28 08:21:33 -0700

    ufiguhrt

--- Functions0228/Functions0228/Main.cpp
+++ Functions0228/Functions0228/Main.cpp
@@ -4,8 +4,8 @@
 using namespace std;
 
 int beep1 = 1800;
-int beep2 = 900;
-int index1 = 1;
+int beep2 = 500;
+int index1 = 0;
 char test1 = 0;
 
 void Annoying();
@@ -26,7 +26,7 @@ void Annoying() {
 		beep1 -= 100;
 		Beep(beep1, beep2);
 	
-	for (int i = 0; i < 25; i++)
+	for (int i = 0; i < 6; i++)
 	MessageBox(NULL, "levy's ugly", "boi", MB_OK);
 
 	}
