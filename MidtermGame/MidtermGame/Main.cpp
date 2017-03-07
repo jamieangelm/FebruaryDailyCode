786471bc2eb48778bdb620a139b718fc0627ca1a
Game110AM <Game110AM@605-110-STU19>
2017-03-07 09:48:54 -0700

    kuhdfg

--- MidtermGame/MidtermGame/Main.cpp
+++ MidtermGame/MidtermGame/Main.cpp
@@ -19,92 +19,86 @@ int main() {
 	while (input != 'q') {
 		switch (room) {
 		case 1:
-			cout << "welcome! you're in room 1! you can go east(e)!";
+			cout << "welcome! you're in room 1! you can go south(s)!";
 			cin >> input;
-			if (input == 'e')
+			if (input == 's')
 				room = 2;
 			else
 				cout << "invalid input" << endl;
 			break;
 
 		case 2:
-			cout << "you're in room 2, where you found an apple! you can go east(e) or south (s)!";
+			cout << "you're in room 2, where you found an apple! you can go east(e)!";
 			cin >> input;
 			if (input == 'e')
 				room = 3;
-			if (input == 's')
-				room = 5;
 			break;
 
 		case 3:
-			cout << "you're in room 3! you can go west(w) or south (s)!";
+			cout << "you're in room 3! you can go east (e) or west (w)!";
 			cin >> input;
+			if (input == 'e')
+				room = 4;
 			if (input == 'w')
 				room = 2;
-			if (input == 's')
-				room = 4;
 			break;
 
 		case 4:
-			cout << "you're in room 4, where you found a key! you can go north(n) or west(w)!";
+			cout << "you're in room 4, where you found a key! you can go west (w) or south (s)!";
 			cin >> input;
-			if (input == 'n')
-				room = 3;
 			if (input == 'w')
+				room = 3;
+			if (input == 's')
 				room = 5;
 			break;
 
 		case 5:
-			cout << "you're in room 5! you can go north(n), east(e), or west(w)!";
+			cout << "you're in room 5! you can go north(n) or south (s)!";
 			cin >> input;
 			if (input == 'n')
-				room = 2;
-			if (input == 'e')
 				room = 4;
-			if (input == 'w')
+			if (input == 's')
 				room = 6;
 			break;
 
 		case 6:
-			cout << "you're in room 6! you can go north(n), east(e), or south (s)!";
+			cout << "you're in room 6! you can go north(n) or west (w)!";
 			cin >> input;
 			if (input == 'n')
-				room = 1;
-			if (input == 'e')
 				room = 5;
-			if (input == 's')
+			if (input == 'w')
 				room = 7;
 			break;
 
 		case 7:
-			cout << "you're in room 7! you see a door that needs a key!";
+			cout << "you're in room 7! you can go west (w) or east (e)!";
 			cin >> input;
-			if (input == 'n')
-				room = 1;
 			if (input == 'w')
-				room = 5;
+				room = 8;
+			if (input == 'e')
+				room = 6;
 			break;
 
 		case 8:
-			cout << "you're in room 7! you see a door that needs a key!";
+			cout << "you're in room 8! you can go south (s) or east (e)!";
 			cin >> input;
-			if (input == 'n')
-				room = 1;
-			if (input == 'w')
-				room = 5;
+			if (input == 's')
+				room = 9;
+			if (input == 'e')
+				room = 7;
 			break;
 
 		case 9:
-			cout << "you're in room 7! you see a door that needs a key!";
+			cout << "you're in room 9! you can go north (n) or south (s)!";
 			cin >> input;
 			if (input == 'n')
-				room = 1;
-			if (input == 'w')
-				room = 5;
+				room = 8;
+			if (input == 's')
+				room = 10;
 			break;
 
 		case 10:
-			cout << "you're in room 7! you see a door that needs a key!";
+			cout << "you're in room 10! you see a door that needs a key!";
 			cin >> input;
 			if (input == 'n')
 				room = 1;
